999c96b1602752117d562e3ccc530feaac6dbe91
eugene.k <ekrotenko.ua@gmail.com>
2019-10-20 18:01:02 +0300

    remove dist and update scripts

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 const serveStatic = require("serve-static");
 
 const app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname, "/dist")));
 
 const port = process.env.port || 5000;
 
